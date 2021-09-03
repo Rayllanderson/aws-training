40f35fe0ec206e06687b3e0e8348f5e60464afef
Rayllanderson <rayllanderson@gmail.com>
2021-09-03 17:13:11 -0300

    GET/pacientes/id

--- api-gateway/handler.js
+++ api-gateway/handler.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const pacientes = [
-  {id: 1, nome: 'Kaguya Shinomiya', dataNascimento: '2004-01-01'},
-  {id: 2, nome: 'Hayasaka Ai', dataNascimento: '2003-04-02'},
-  {id: 3, nome: 'Chika Fujiwara', dataNascimento: '2003-03-03'}
+  { id: 1, nome: 'Kaguya Shinomiya', dataNascimento: '2004-01-01' },
+  { id: 2, nome: 'Hayasaka Ai', dataNascimento: '2003-04-02' },
+  { id: 3, nome: 'Chika Fujiwara', dataNascimento: '2003-03-03' }
 ]
 
 module.exports.listarPacientes = async (event) => {
@@ -11,6 +11,29 @@ module.exports.listarPacientes = async (event) => {
     statusCode: 200,
     body: JSON.stringify(pacientes),
   };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.buscaPorId = async (event) => {
+  const { pacienteId } = event.pathParameters
+
+  const pacienteEncontrado = pacientes.find(paciente => paciente.id == pacienteId)
+
+  if (pacienteEncontrado) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(pacienteEncontrado),
+    };
+  }else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({error: "Não encontrado"}),
+    };
+  }
+
+
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
