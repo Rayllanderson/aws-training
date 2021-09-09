6627ea293b601dfdcb4f1662b869df7ba29c1fcf
Rayllanderson <rayllanderson@gmail.com>
2021-09-09 11:50:59 -0300

    Acessando dynamodb - put_item

--- lambda/python/dynamodb.py
+++ lambda/python/dynamodb.py
@@ -4,7 +4,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('planets')
 
-def lambda_handler(event, context):
+def lambda_handler_get_item(event, context):
   response = table.get_item(
     Key={
         'id': 'jupiter'
@@ -13,4 +13,17 @@ def lambda_handler(event, context):
   return {
     'statusCode': 200,
     'body': response
+  }
+
+
+def lambda_handler_put_item(event, context):
+  table.put_item(
+      Item={
+          'id': 'Saturn',
+          'description': 'the sixth planet from the Sun and the second-largest planet in our solar system'
+      }
+  )
+  return {
+      'statusCode': 201,
+      'body': 'Item added'
   }
